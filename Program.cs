97075eca3f46f457111a4ba75b531a1cf5e17dc9
RotarRobert183 <rotarmartin95@gmail.com>
2025-12-12 14:38:11 +0200

    fixed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,56 @@
+using HCI.AIAssistant.API.Services;
+using Microsoft.Extensions.Options;
+using HCI.AIAssistant.API.Managers;
+using Azure.Identity;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+var keyVaultName = builder.Configuration
+  [$"AppConfigurations{ConfigurationPath.KeyDelimiter}KeyVaultName"];
+var secretsPrefix = builder.Configuration
+   [$"AppConfigurations{ConfigurationPath.KeyDelimiter}SecretsPrefix"];
+if (string.IsNullOrWhiteSpace(keyVaultName))
+{
+    throw new ArgumentNullException("KeyVaultName", "KeyVaultName is missing.");
+}
+if (string.IsNullOrWhiteSpace(secretsPrefix))
+{
+    throw new ArgumentNullException("SecretsPrefix", "SecretsPrefix is missing.");
+}
+var keyVaultUri = new Uri(
+    $"https://{keyVaultName}.vault.azure.net/"
+);
+builder.Configuration.AddAzureKeyVault(
+    keyVaultUri, new DefaultAzureCredential(), new CustomSecretManager(secretsPrefix)
+);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CORS",
+    policy =>
+    {
+        policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+
+
 // Add services to the container.
+builder.Services.Configure<SecretsService>(builder.Configuration.GetSection("Secrets"));
+builder.Services.Configure<AppConfigurationsService>(builder.Configuration.GetSection("AppConfigurations"));
+
+// Add services to the container. 
+builder.Services.AddSingleton<ISecretsService>(
+    provider => provider.GetRequiredService<IOptions<SecretsService>>().Value
+);
+builder.Services.AddSingleton<IAppConfigurationsService>(
+    provider => provider.GetRequiredService<IOptions<AppConfigurationsService>>().Value
+);
+
+builder.Services.AddSingleton<IParametricFunctions, ParametricFunctions>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -11,6 +61,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+
+app.UseCors("CORS");
 app.UseSwagger();
 app.UseSwaggerUI();
 if (app.Environment.IsDevelopment())
@@ -21,5 +73,16 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.EndPoint);
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.Key);
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.AIAssistantSecrets?.Id);
+Console.WriteLine(app.Services.GetService<ISecretsService>()?.IoTHubSecrets?.ConnectionString);
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?
+ .KeyVaultName);
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?
+ .SecretsPrefix);
+Console.WriteLine(app.Services.GetService<IAppConfigurationsService>()?
+ .IoTDeviceName);
 
 app.Run();
